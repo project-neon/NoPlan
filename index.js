4b88821222c13a5cdbf81ae14a0fd10392b5a110
Ivan Seidel <ivanseidel@gmail.com>
2016-03-05 16:18:58 -0300

    Created views folder

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var colors = require('colors');
+
+console.log('hey!'.red);
+
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
