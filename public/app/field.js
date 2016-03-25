2987424d427808c0fc57170fc3e4369cb21883f6
Matheus Rodrigues <matt_rodrigues@live.com>
2016-03-25 11:25:06 -0300

    Fixed ball diameter

--- public/app/field.js
+++ public/app/field.js
@@ -15,7 +15,7 @@ var fieldOffset =  10; // padding between field limits and canvas size
 var canvasHeight = 600;
 var canvasWidth  = fieldWidth*canvasHeight/fieldHeight;
 var middleRadius = (40*canvasHeight/fieldHeight)/2;
-var ballSize     = 0.427*canvasHeight/fieldHeight;
+var ballSize     = 4.27*canvasHeight/fieldHeight;
 
 // create a Matter.js engine
 var engine = Engine.create({
@@ -31,18 +31,18 @@ var engine = Engine.create({
 
 // NOTE: Create field points to create vector
 var fieldVertices = [
-  { x: 10, y: 0}, //left-bottom corner
-  { x: 160, y: 0}, //right-bottom corner
-  { x: 160, y: 45}, //right goal-line (bottom)
-  { x: 170, y: 45}, //right goal-endline (bottom)
-  { x: 170, y: 85}, //right goal-endline (top)
-  { x: 160, y: 85}, //right goal-line (top)
-  { x: 160, y: 130}, //right-top corner
-  { x: 10, y: 130}, //left-top corner
-  { x: 10, y: 85}, //left goal-line (top)
-  { x: 0, y: 85}, //left goal-endline (top)
-  { x: 0, y: 45}, //left goal-endline (bottom)
-  { x: 10, y: 45} //left goal-line (bottom)
+  { x:  10, y:   0 }, //left-bottom corner
+  { x: 160, y:   0 }, //right-bottom corner
+  { x: 160, y:  45 }, //right goal-line (bottom)
+  { x: 170, y:  45 }, //right goal-endline (bottom)
+  { x: 170, y:  85 }, //right goal-endline (top)
+  { x: 160, y:  85 }, //right goal-line (top)
+  { x: 160, y: 130 }, //right-top corner
+  { x:  10, y: 130 }, //left-top corner
+  { x:  10, y:  85 }, //left goal-line (top)
+  { x:   0, y:  85 }, //left goal-endline (top)
+  { x:   0, y:  45 }, //left goal-endline (bottom)
+  { x:  10, y:  45 }  //left goal-line (bottom)
 ];
 
 // NOTE: Scales and loop to resize field array_size
@@ -62,12 +62,12 @@ var fieldLimits = Body.create({
 });
 
 // NOTE: Creates the ball
-var fieldBall = Bodies.circle(canvasWidth/2, canvasHeight/2, ballSize);
+var fieldBall = Bodies.circle(canvasWidth/2, canvasHeight/2, ballSize/2);
 
 // NOTE: add all of the bodies to the world
 World.add(engine.world, MouseConstraint.create(engine));
 World.add(engine.world, [fieldLimits, fieldBall]); // NOTE: NOT WORKING, ONLY SHOW THE FIELD LIMITS (STATIC ELEMENTS)
-World.add(engine.world, [fieldBall]); // NOTE: SHOW NON STATIC ELEMENTS (THE BALL)
+// World.add(engine.world, [fieldBall]); // NOTE: SHOW NON STATIC ELEMENTS (THE BALL)
 
 // NOTE: Change world gravity on y axis to zero
 engine.world.gravity.y = 0;
