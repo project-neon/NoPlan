6eb30aa4645c5ba509cf153de66ecf02be8b0b15
Matheus Rodrigues <matt_rodrigues@live.com>
2016-03-25 02:34:31 -0300

    Added field limits polygon

--- public/app/field.js
+++ public/app/field.js
@@ -1,6 +1,8 @@
 // Matter.js module aliases
 var Engine = Matter.Engine,
     World  = Matter.World,
+    MouseConstraint = Matter.MouseConstraint,
+    Body = Matter.Body,
     Bodies = Matter.Bodies;
 
 // NOTE: Values of VSS Field in centimeters
@@ -12,7 +14,7 @@ var fieldOffset =  10; // padding between field limits and canvas size
 // NOTE: Canvas size in pixels
 var canvasHeight = 600;
 var canvasWidth  = fieldWidth*canvasHeight/fieldHeight;
-var diameter     = 40*canvasHeight/fieldHeight;
+var middleRadius = (40*canvasHeight/fieldHeight)/2;
 var ballSize     = 0.427*canvasHeight/fieldHeight;
 
 // create a Matter.js engine
@@ -27,13 +29,48 @@ var engine = Engine.create({
   }
 });
 
-var fieldLeft   = Bodies.rectangle(canvasWidth/2 - canvasWidth/4 + 5, canvasHeight/2, canvasWidth/2 - 10, canvasHeight-10, { isStatic: true });
-var fieldRight  = Bodies.rectangle(canvasWidth/2 + canvasWidth/4 - 5, canvasHeight/2, canvasWidth/2 - 10, canvasHeight-10, { isStatic: true });
-var fieldMiddle = Bodies.circle(canvasWidth/2, canvasHeight/2, diameter/2, { isStatic: true });
-var fieldBall   = Bodies.circle(canvasWidth/2, canvasHeight/2, ballSize, { isStatic: true });
-// add all of the bodies to the world
-// World.add(engine.world, [boxA, boxB, ground]);
-World.add(engine.world, [fieldLeft, fieldRight, fieldMiddle, fieldBall]);
+// NOTE: Create field points to create vector
+var fieldVertices = [
+  { x: 10, y: 0}, //left-bottom corner
+  { x: 160, y: 0}, //right-bottom corner
+  { x: 160, y: 45}, //right goal-line (bottom)
+  { x: 170, y: 45}, //right goal-endline (bottom)
+  { x: 170, y: 85}, //right goal-endline (top)
+  { x: 160, y: 85}, //right goal-line (top)
+  { x: 160, y: 130}, //right-top corner
+  { x: 10, y: 130}, //left-top corner
+  { x: 10, y: 85}, //left goal-line (top)
+  { x: 0, y: 85}, //left goal-endline (top)
+  { x: 0, y: 45}, //left goal-endline (bottom)
+  { x: 10, y: 45} //left goal-line (bottom)
+];
 
-// run the engine
+// NOTE: Scales and loop to resize field array_size
+var yScale = 590/130;
+var xScale = (fieldWidth*590/fieldHeight)/170;
+for(var i=0; i<fieldVertices.length; i++){
+  fieldVertices[i].x *= xScale;
+  fieldVertices[i].y *= yScale;
+}
+
+// NOTE: Create field limits body with Matter.Body
+var fieldLimits = Body.create({
+  position: { x: canvasWidth/2, y: canvasHeight/2 },
+  vertices: fieldVertices,
+  isStatic: true,
+  label: 'FieldLimits'
+});
+
+// NOTE: Creates the ball
+var fieldBall = Bodies.circle(canvasWidth/2, canvasHeight/2, ballSize);
+
+// NOTE: add all of the bodies to the world
+World.add(engine.world, MouseConstraint.create(engine));
+World.add(engine.world, [fieldLimits, fieldBall]); // NOTE: NOT WORKING, ONLY SHOW THE FIELD LIMITS (STATIC ELEMENTS)
+World.add(engine.world, [fieldBall]); // NOTE: SHOW NON STATIC ELEMENTS (THE BALL)
+
+// NOTE: Change world gravity on y axis to zero
+engine.world.gravity.y = 0;
+
+// NOTE: run the engine
 Engine.run(engine);
