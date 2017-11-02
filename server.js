515b57c0b17f07e8e05f03de45d04f41888cf781
João Pedro Vilas Boas <joaopedrovbs@gmail.com>
2017-11-02 15:16:20 -0200

    Add server static files

--- server.js
+++ server.js
@@ -2,95 +2,42 @@ const _ = require('lodash')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const SerialPort = require('serialport')
-
+const express = require('express')
+const app = express()
+const path = require('path')
 require('draftlog').into(console)
 
 const Match = require('./lib/Match')
-
 const players = require('require-smart')('./players')
 
-const PORT = process.env.PORT || 10002
-const HOST = process.env.HOST || '224.5.23.2'
+const PORT = 10006
+const HOST = '224.5.23.2'
 
 const sleep = ms => new Promise((res, rej) => setTimeout(res, ms))
 
 const TAG = 'server'
+
 async function startup(){
   console.info(TAG, chalk.yellow('startup'))
   
   let match = new Match({
     vision: { PORT, HOST },
     robots: {
-      red: {visionId: 1, radioId: 1, class: players.Attacker}
+      fretado: {visionId: 2, radioId: 2, class: players.Attacker},
+      piso_vermelho: {visionId: 0, radioId: 3, class: players.Attacker},
+      torre_do_relogio: {visionId: 4, radioId: 1, class: players.Attacker}
     },
     driver: {
       port: await getPort('/dev/tty.usbserial-A10252WB'),
-      // debug: true,
+      debug: false,
       baudRate: 500000,
     }
   })
-
   await match.init()
   console.log('Listening in:', PORT)
-
-  // match.vision.on('data', console.log)
-
-  console.log('front ..')
-  let stats = console.draft('...')
-  let statsTimes1 = console.draft('...')
-  let statsTimes2 = console.draft('...')
-  let startTime = Date.now()
-  let sent = 0
-  let failed = 0
-  let time = 0
-  let times = {}
-  while(1) {
-    try {
-      let begin = Date.now()
-      let ps = [
-        match.driver.send(':1;1;0;0'), // :1;1;0;0
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-        match.driver.send(':1;1;0;0'),
-      ]
-
-      // wait all promises
-      for (let p of ps) try { 
-        sent++
-        await p 
-      } catch (e) {
-        failed++
-        // console.log(e)
-      }
-
-      time = Date.now() - begin
-      times[time] = times[time] ? times[time] + 1 : 1
-    } catch (e) {}
-
-    let dropedPercentage = (failed / sent * 100).toFixed(2)
-    let pps = ((sent) / ((Date.now() - startTime) / 1000)).toFixed(1)
-    
-    stats(
-      chalk.bgGreen.white(' STATS '),
-      chalk.red(`failed: ${failed}\t`),
-      chalk.green(`sent: ${sent}\t`),
-      chalk.yellow(`${dropedPercentage}%\t`),
-      chalk.white(`${time} ms\t`),
-      chalk.white(`${pps} packets/s\t`))
-
-    statsTimes1(Object.keys(times))
-    statsTimes2(Object.values(times))
-    // await match.driver.send(':1;1;40;0')
-    // await sleep(500)
-    // await match.driver.send(':1;1;0;90')
-    // await sleep(1000)
-  }
-  console.log('front ok')
-
+  app.listen(80, function () {
+  console.log('View listening on port 80!')
+}) 
 }
 
 process.on('unhandledRejection', (e) => {
@@ -100,6 +47,11 @@ process.on('unhandledRejection', (e) => {
 })
 
 startup()
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'/static/index.html'))
+})
+
 
 async function getPort(prefered) {
   let ports = await SerialPort.list()
@@ -139,12 +91,4 @@ async function getPort(prefered) {
 //     this.state.setState(data)
 //   })
 // }
-
-const pasync = {
-  all: (ps) => {
-
-  },
-
-
-}
 
