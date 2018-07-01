5fdd4986e8b704f17ef5f6dd23df3d6d2ec4cbe1
Alexsandro <alexsandro2134@gmail.com>
2018-07-01 12:10:35 -0300

    Update ball and ballspeed only once (at coach)

--- players/IntentionPlayer.js
+++ players/IntentionPlayer.js
@@ -149,44 +149,44 @@ module.exports = class IntentionPlayer extends BasePlayer {
       await this.send(0,0,0)
       return
     }
-    if(this.frame){
-      let frame = this.frame
-      // Update ball position
-      // console.log(frame)
-      if(frame.balls[0]){
-        this.lastBall = this.ball
+    // if(this.frame){
+    //   let frame = this.frame
+    //   // Update ball position
+    //   // console.log(frame)
+    //   if(frame.balls[0]){
+    //     this.lastBall = this.ball
 
-        let dt = this.frame.t_capture - this.lastBallTimestamp
-        this.lastBallTimestamp = this.frame.t_capture
+    //     let dt = this.frame.t_capture - this.lastBallTimestamp
+    //     this.lastBallTimestamp = this.frame.t_capture
 
-        this.ball = frame.balls[0]
+    //     this.ball = frame.balls[0]
 
-        if (dt && dt < 0.04 && this.lastBall) {
+    //     if (dt && dt < 0.04 && this.lastBall) {
 
-          this._ballSpeedsRaw.unshift({
-            x: (this.ball.x - this.lastBall.x) / dt,
-            y: (this.ball.y - this.lastBall.y) / dt,
-          })
+    //       this._ballSpeedsRaw.unshift({
+    //         x: (this.ball.x - this.lastBall.x) / dt,
+    //         y: (this.ball.y - this.lastBall.y) / dt,
+    //       })
 
-          this._ballSpeedsRaw = this._ballSpeedsRaw.slice(0, 10)
+    //       this._ballSpeedsRaw = this._ballSpeedsRaw.slice(0, 10)
 
-          let avgSpeed = this._ballSpeedsRaw.reduce((last, speed) => {
-            return {x: last.x + speed.x, y: last.y + speed.y}
-          }, {x: 0, y: 0})
+    //       let avgSpeed = this._ballSpeedsRaw.reduce((last, speed) => {
+    //         return {x: last.x + speed.x, y: last.y + speed.y}
+    //       }, {x: 0, y: 0})
 
-          avgSpeed.x = avgSpeed.x / this._ballSpeedsRaw.length
-          avgSpeed.y = avgSpeed.y / this._ballSpeedsRaw.length
+    //       avgSpeed.x = avgSpeed.x / this._ballSpeedsRaw.length
+    //       avgSpeed.y = avgSpeed.y / this._ballSpeedsRaw.length
 
-          this.ballSpeed = avgSpeed
+    //       this.ballSpeed = avgSpeed
 
-          // console.log(dt + '\t' + avgSpeed.x.toFixed(0) + '\t' + avgSpeed.y.toFixed(0))
-        }
-        // Compute ball average speed
-        // let sum = this.lastBalls.reduce((prev, ball) => {prev})
-      }
-    } else{
-      console.error("Frame not present in IntentionPlayer")
-    }
+    //       // console.log(dt + '\t' + avgSpeed.x.toFixed(0) + '\t' + avgSpeed.y.toFixed(0))
+    //     }
+    //     // Compute ball average speed
+    //     // let sum = this.lastBalls.reduce((prev, ball) => {prev})
+    //   }
+    // } else{
+    //   console.error("Frame not present in IntentionPlayer")
+    // }
     await this.loop()
 
     // Prepare input for intentions
