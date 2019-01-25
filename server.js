5daab6f9b9510567b6257e26311a255cdd7c4e08
Alexsandro Francisco <alexsandro2134@gmail.com>
2019-01-25 19:12:33 -0200

    Start to make more compreehensive logs

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const HOST = '224.5.23.2'
 
 const sleep = ms => new Promise((res, rej) => setTimeout(res, ms))
 const TAG = 'server'
+
+const RUN_MODES = {1: 'REAL_LIFE', 2: 'SIMULATED', 3: 'ASSIST'}
 /*
 Variavel RUN_MODE args:
   1: REAL_LIFE, Executa o fluxo completo, usado para partidas reais
@@ -26,7 +28,9 @@ Variavel RUN_MODE args:
 */
 
 const run_mode = process.env.RUN_MODE
-assert.notEqual(run_mode, null)
+
+assert.notEqual(run_mode, null, chalk.red('MISSING PARAMETER: RUN_MODE'))
+assert.ok(run_mode in RUN_MODES, chalk.red('WRONG PARAMETER: RUN_MODE, must be 1, 2 or 3'))
 
 const isSimulated = run_mode==2
 const usePrediction = false
@@ -34,8 +38,9 @@ const noStation = run_mode==3
 
 async function startup(){
   console.info(TAG, chalk.yellow('startup'))
-  console.info(TAG, chalk.yellow('isSimulated'), isSimulated)
-  console.info(TAG, chalk.yellow('usePrediction'), usePrediction)
+  console.info(TAG, chalk.yellow('Run mode: '), chalk.green(RUN_MODES[run_mode]))
+  // FIXME: Se a simulação não esta confiavel vale tentar usar?
+  // console.info(TAG, chalk.yellow('usePrediction'), usePrediction)
 
   let MatchClass = (isSimulated ? MatchSimulated : Match)
 
