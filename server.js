4b256cebf4ec445d2b23f08b81b45bc88ef0df13
Gabriel <mendes.gabriel95@gmail.com>
2019-01-27 16:38:01 -0200

    Revert "Documentation Server and change init parameters"

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const _ = require('lodash')
 const chalk = require('chalk')
-const assert = require('assert')
 const inquirer = require('inquirer')
 const SerialPort = require('serialport')
 const comm = require('./lib/Comm.js')
@@ -16,32 +15,18 @@ const PORT = 10006
 const HOST = '224.5.23.2'
 
 const sleep = ms => new Promise((res, rej) => setTimeout(res, ms))
+
 const TAG = 'server'
 
-const RUN_MODES = {1: 'REAL_LIFE', 2: 'SIMULATED', 3: 'ASSIST'}
-/*
-Variavel RUN_MODE args:
-  1: REAL_LIFE, Executa o fluxo completo, usado para partidas reais
-  2: SIMULATED, modo simulado, usando stack SIR-LAB
-  3: ASSIST   , modo assistido, onde o NoPlan recebe dados do
-  SSL-Vision mas não envia para lugar algum
-*/
-
-const run_mode = process.env.RUN_MODE
-
-assert.notEqual(run_mode, null, chalk.red('MISSING PARAMETER: RUN_MODE'))
-assert.ok(run_mode in RUN_MODES, chalk.red('WRONG PARAMETER: RUN_MODE, must be 1, 2 or 3'))
-
-const isSimulated = run_mode==2
+const isSimulated = !!process.env.SIMULATED
 const usePrediction = false
-const noStation = run_mode==3
+const noStation = process.env.NO_STATION | false
 
 async function startup(){
-  console.info(TAG, chalk.yellow('Startup'))
-  console.info(TAG, chalk.yellow('Run mode: '), chalk.green(RUN_MODES[run_mode]))
-  // FIXME: Se a simulação não esta confiavel vale tentar usar?
-  // console.info(TAG, chalk.yellow('usePrediction'), usePrediction)
-
+  console.info(TAG, chalk.yellow('startup'))
+  console.info(TAG, chalk.yellow('isSimulated'), isSimulated)
+  console.info(TAG, chalk.yellow('usePrediction'), usePrediction)
+    
   let MatchClass = (isSimulated ? MatchSimulated : Match)
 
   if(noStation) {
@@ -60,14 +45,14 @@ async function startup(){
     },
     driver: {
       port: ( (isSimulated || noStation) ? null : await getPort('/dev/ttyUSB0')),
-      debug: false,
+      debug: true,
       baudRate: 115200,
     }
   })
 
   await match.init()
   console.log('Listening in:', PORT)
-
+  
   await comm(match, {PORT:8080})
 
 }
@@ -89,7 +74,7 @@ async function getPort(prefered) {
   console.log(`Port '${prefered}' not available`)
 
   let answer = await inquirer.prompt({
-    type: 'list',
+    type: 'list', 
     name: 'port',
     choices: _.map(ports, 'comName'),
     message: 'Select Cursor port'
