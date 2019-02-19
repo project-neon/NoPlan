3fe3df3b0eccd267684a0deec63fd61d957baeb2
Alexsandro Francisco <alexsandro2134@gmail.com>
2019-02-19 20:20:04 -0300

    Make this work

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const fs = require("fs");
 const _ = require('lodash')
 const chalk = require('chalk')
 const assert = require('assert')
 const inquirer = require('inquirer')
-const SerialPort = require('serialport')
 const comm = require('./lib/Comm.js')
 require('draftlog').into(console)
 
@@ -23,6 +21,8 @@ const sleep = ms => new Promise((res, rej) => setTimeout(res, ms))
 const TAG = 'server'
 
 const RUN_MODES = {1: 'REAL_LIFE', 2: 'SIMULATED', 3: 'ASSIST'}
+
+const config = null
 /*
 Variavel RUN_MODE args:
   1: REAL_LIFE, Executa o fluxo completo, usado para partidas reais
@@ -31,7 +31,7 @@ Variavel RUN_MODE args:
   SSL-Vision mas não envia para lugar algum
 */
 
-const run_mode = process.env.RUN_MODE
+const run_mode = parseInt(process.env.RUN_MODE)
 
 assert.notEqual(run_mode, null, chalk.red('MISSING PARAMETER: RUN_MODE'))
 assert.ok(run_mode in RUN_MODES, chalk.red('WRONG PARAMETER: RUN_MODE, must be 1, 2 or 3'))
@@ -39,32 +39,37 @@ assert.ok(run_mode in RUN_MODES, chalk.red('WRONG PARAMETER: RUN_MODE, must be 1
 async function startup(){
   console.info(TAG, chalk.yellow('Startup'))
   console.info(TAG, chalk.yellow('Run mode: '), chalk.green(RUN_MODES[run_mode]))
-  // FIXME: Se a simulação não esta confiavel vale tentar usar?
-  // console.info(TAG, chalk.yellow('usePrediction'), usePrediction)
+  const config = require('./config.json')
+
+  let visionImpl = config.vision
+  let driverImpl = config.driver
 
   let MatchClass = null
   switch (run_mode) {
     case 1:
       MatchClass = Match
+      break
     case 2:
       MatchClass = MatchSimulated
+      break
     case 3:
       MatchClass = MatchVSSSimulated
-
+      break
   }
 
   let match = new MatchClass({
     // Vision: Dados referentes ao input de dados da visão
     // Coach: Dados de configuração do Coach que ira orquestrar a partida
     // Driver: dados referentes ao output de dados de envio para os robos
-    vision: { PORT, HOST },
+    vision: {impl: visionImpl, params: config[visionImpl]},
+    driver: {impl: driverImpl, params: config[driverImpl]},
     coach: Coach,
     robotsProperties: {robot_1: {vision_id: 1, radio_id:1}},
-    driver: {
-      port: (run_mode in [2, 3] ? null : await getPort('/dev/ttyUSB0')),
-      debug: false,
-      baudRate: 115200,
-    }
+    // driver: {
+    //   port: (run_mode in [2, 3] ? null : await getPort('/dev/ttyUSB0')),
+    //   debug: false,
+    //   baudRate: 115200,
+    // }
   })
 
   await match.init()
@@ -81,21 +86,3 @@ process.on('unhandledRejection', (e) => {
 })
 
 startup()
-
-async function getPort(prefered) {
-  let ports = await SerialPort.list()
-  let found = _.find(ports, {comName: prefered})
-  if (found)
-    return prefered
-
-  console.log(`Port '${prefered}' not available`)
-
-  let answer = await inquirer.prompt({
-    type: 'list',
-    name: 'port',
-    choices: _.map(ports, 'comName'),
-    message: 'Select Cursor port'
-  })
-
-  return answer.port
-}
