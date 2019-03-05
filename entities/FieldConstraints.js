b61765031251302691f60580b3508931edf12388
Gabriel Mendes <mendes.gabriel95@gmail.com>
2019-03-05 15:05:45 -0300

    Add side small areas

--- entities/FieldConstraints.js
+++ entities/FieldConstraints.js
@@ -5,11 +5,23 @@ modeule.exports = {
     x1: 700,
     y1: -2000
   },
+  SMALL_AREA_LEFT = {
+    x0: -550,
+    y0: 301,
+    x1: -700,
+    y1: 602
+  },
   RIGHT = {
     x0: 0,
     y0: 300,
     x1: 700,
     y1: 2000
+  },
+  SMALL_AREA_RIGHT = {
+    x0: -550,
+    y0: 301,
+    x1: -700,
+    y1: 602
   },
   CENTER = {
     x0: 0,
@@ -17,8 +29,8 @@ modeule.exports = {
     x1: 700,
     y1: 602
   },
-  SMALL_AREA = {
-    x0: 150,
+  SMALL_AREA_CENTER = {
+    x0: -550,
     y0: 301,
     x1: -700,
     y1: 602
