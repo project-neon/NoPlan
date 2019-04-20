b7d9bc0de6a12527a69a94f1b4e04eb9370f81d9
Alexsandro Francisco <alexsandro2134@gmail.com>
2019-04-20 16:25:19 -0300

    Finish tests with multiple

--- players/tests/PointIntentionPlayer.js
+++ players/tests/PointIntentionPlayer.js
@@ -8,7 +8,7 @@ const FORWARD_SPEED = 50
 module.exports = class PointIntentionPlayer extends IntentionPlayer {
   setup(){
     this.addIntetion(new PointIntention('test', {
-      target: {x: 200, y: 0},
+      target: {x: 0, y: 0},
       theta: Vector.direction("left"),
       radius: 100,
       decay: TensorMath.new.finish,
