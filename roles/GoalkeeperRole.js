0225f50f7c450d9a18266bc9defeb121984ee738
Alexsandro Francisco <alexsandro2134@gmail.com>
2020-02-11 22:44:46 -0300

    Simple defensive goalkeeper

--- roles/GoalkeeperRole.js
+++ roles/GoalkeeperRole.js
@@ -1,6 +1,4 @@
-const GoalKeeper = require('../players/experimental/GoalkeeperMain')
-const GoalKeeperSides = require('../players/experimental/GoalkeeperSides')
-const GoalKeeperPush = require('../players/experimental/GoalkeeperPush')
+const GoalKeeperMain = require('../players/experimental/GoalkeeperMain2')
 const FieldConstraints = require('../entities/FieldConstraints')
 
 
@@ -12,9 +10,7 @@ module.exports = class GoalKeeperRole {
 
     async init() {
         this.plays = {
-            main: new GoalKeeper(0, this.match, null),
-            push: new GoalKeeperPush(0, this.match, null),
-            side: new GoalKeeperSides(0, this.match, null)
+            main: new GoalKeeperMain(0, this.match, null),
         }
     }
 
@@ -26,35 +22,6 @@ module.exports = class GoalKeeperRole {
     }
 
     decidePlay(robot, data) {
-        /*
-        O Goleiro tem 4 tipos de comportamentos bem definidos:
-
-        1: Se não houver bola em campo então goleiro fica no centro do gol (play main)
-        2: Se a bola estiver dentro da pequena area, empurre a bola para longe (play push)
-        3: Se a bola estiver nas laterais da pequena area, proteja a trave mais proxima (play sides)
-        4: caso contrario, fique no centro do gol (play main)
-        */
-        let ball = null
-        if (data)
-            ball = data.cleanData.ball || null
-        if (!ball) {
-            robot.runningPlay = this.plays.main
-            this.plays.main.setRobot(robot)
-        }
-        if (this.insideFieldConstraint(FieldConstraints.SMALL_AREA_CENTER, ball)) {
-            robot.runningPlay = this.plays.push
-            this.plays.push.setRobot(robot)
-        } else if (
-            this.insideFieldConstraint(FieldConstraints.SMALL_AREA_LEFT, ball)
-            || 
-            this.insideFieldConstraint(FieldConstraints.SMALL_AREA_RIGHT, ball)
-        ) {
-            robot.runningPlay = this.plays.side
-            this.plays.side.setRobot(robot)
-        } else {
-            robot.runningPlay = this.plays.main
-            this.plays.main.setRobot(robot)
-        }
 
         robot.runningPlay = this.plays.main
         this.plays.main.setRobot(robot)
