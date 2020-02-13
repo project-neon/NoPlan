fcd0b2a773eae31d35d881f7a7b9c9377c085f66
Alexsandro Francisco <alexsandro2134@gmail.com>
2020-02-12 22:43:00 -0300

    Recreate goalkeeper sides2

--- roles/GoalkeeperRole.js
+++ roles/GoalkeeperRole.js
@@ -1,4 +1,5 @@
 const GoalKeeperMain = require('../players/experimental/GoalkeeperMain2')
+const GoalKeeperSides = require('../players/experimental/GoalkeeperSides2')
 const FieldConstraints = require('../entities/FieldConstraints')
 
 
@@ -11,6 +12,7 @@ module.exports = class GoalKeeperRole {
     async init() {
         this.plays = {
             main: new GoalKeeperMain(0, this.match, null),
+            sides: new GoalKeeperSides(0, this.match, null)
         }
     }
 
@@ -22,9 +24,20 @@ module.exports = class GoalKeeperRole {
     }
 
     decidePlay(robot, data) {
+        
+        const ball = this.match.dataManager.ball
 
-        robot.runningPlay = this.plays.main
-        this.plays.main.setRobot(robot)
+        if (
+            this.insideFieldConstraint(FieldConstraints.LEFT, ball) 
+            || this.insideFieldConstraint(FieldConstraints.RIGHT, ball)
+            ) {
+            robot.runningPlay = this.plays.sides
+            this.plays.sides.setRobot(robot)
+        }else {
+            robot.runningPlay = this.plays.main
+            this.plays.main.setRobot(robot)
+        }
+
 
         console.log('|robot' + robot.radioId + '| decided play: ' + robot.runningPlay.constructor.name)
     }
